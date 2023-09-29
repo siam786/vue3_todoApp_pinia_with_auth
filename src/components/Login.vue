5fd5253806a1fed8abd0031a2e23dd5c701f39d6
siam786e <shohrab.hossain36@gmail.com>
2023-09-29 20:55:34 +0600

    add ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,46 +1,91 @@
 <script setup>
-    import store from '../store/loginRegister';
-    import {reactive} from 'vue'
+import { reactive } from "vue";
+import store from "../store/loginRegister";
 
-    const state = store()
+const state = store();
 
-    const LoginInput = reactive({});
+const LoginInput = reactive({});
 
-    function LoginData(){
-      state.login(LoginInput)
-    }
-
+function LoginData() {
+  state.login(LoginInput);
+}
 </script>
 
 <template>
-   <div class="h-screen w-full flex items-center justify-center bg-blue font-sans" >
-      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col mb-20 mt-20">
-         <div class="mb-4">
-            <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
-               Username
-            </label>
-            <input v-model="LoginInput.email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username"
-               type="email" placeholder="Email">
-         </div>
-         <div class="mb-6">
-            <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
-               Password
-            </label>
-            <input v-model="LoginInput.password" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-               id="password" type="password" placeholder="Enter Password">
-         </div>
-         <div class="flex items-center justify-between">
-            <button @click="LoginData()" class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">
-               Sign In
-            </button>
-            <router-link class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" :to="{name: 'Register'}">
-               Registration
-            </router-link>
-         </div>
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        class="mx-auto h-10 w-auto"
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company"
+      />
+      <h2
+        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      >
+        Sign in to your account
+      </h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div>
+        <label
+          for="email"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Email address</label
+        >
+        <div class="mt-2">
+          <input
+            placeholder="Enter Email"
+            v-model="LoginInput.email"
+            id="email"
+            name="email"
+            type="email"
+            autocomplete="email"
+            required
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
       </div>
-   </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label
+            for="password"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Password</label
+          >
+        </div>
+        <div class="mt-2">
+          <input
+            placeholder="Enter Password"
+            v-model="LoginInput.password"
+            id="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            required
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          @click="LoginData()"
+          type="submit"
+          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-5"
+        >
+          Sign in
+        </button>
+      </div>
+      <router-link
+        class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+        :to="{ name: 'Register' }"
+      >
+        Registration
+      </router-link>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
