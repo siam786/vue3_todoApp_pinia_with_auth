3673d29aa9dbdcd4f07cf4d636d030db827eb92d
siam786e <shohrab.hossain36@gmail.com>
2023-09-30 18:30:36 +0600

    checkbox add in todos

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -18,7 +18,7 @@
 </script>
 
 <template>
-    <div class="flex items-center justify-center w-full h-screen font-sans bg-blue-500">
+    <div class="flex items-center justify-center w-full h-screen font-mono bg-gradient-to-r from-indigo-500 to-blue-500">
       <div class="w-full p-6 m-4 rounded shadow bg-gradient-to-r from-blue-200 to-cyan-200 lg:w-4/4 lg:max-w-lg">
         <div class="mb-4">
           <h1 class="text-4xl text-red-500">Todo App</h1>
@@ -26,7 +26,7 @@
             <input v-model="inputData" class="w-full px-3 py-2 mr-4 border rounded shadow appearance-none text-grey-darker"
               placeholder="Add Todo">
             <button @click="insertInputData()"
-              class="p-2 border-2 rounded flex-no-shrink text-teal border-red hover:text-white hover:bg-red-500">Add</button>
+              class="p-2 border-2 border-red-500 rounded flex-no-shrink text-teal hover:text-white hover:bg-red-500">Add</button>
           </div>
         </div>
         <div>
@@ -34,21 +34,27 @@
           
            <div class="flex items-center mb-4" v-for="(project, index) in store.inCompleteProject" :key="index">
             <p class="w-full text-grey-darkest">{{ project.name }}</p>
-            <button
-             @click="store.doneAsComplete(project.id)" type="button" class="p-2 ml-4 mr-2 border-2 rounded flex-no-shrink hover:text-white text-green border-green hover:bg-blue-500">Done</button>
+            
+            <div @click="store.doneAsComplete(project.id)" class="flex items-center justify-center w-6 h-6 p-1 mr-2 bg-white shadow">
+                <input type="checkbox"  unchecked>
+                <svg class="hidden w-4 h-4 text-green-600 pointer-events-none" viewBox="0 0 172 172"><g fill="none" stroke-width="none" stroke-miterlimit="10" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode:normal"><path d="M0 172V0h172v172z"/><path d="M145.433 37.933L64.5 118.8658 33.7337 88.0996l-10.134 10.1341L64.5 139.1341l91.067-91.067z" fill="currentColor" stroke-width="1"/></g></svg>
+            </div>
+             
             <button type="button" @click="store.deleteProject(project.id)"
-              class="p-2 ml-2 border-2 rounded flex-no-shrink text-red border-red hover:text-white hover:bg-blue-500">Remove</button>
+              class="p-2 ml-2 border-2 border-red-500 rounded flex-no-shrink text-red hover:text-white hover:bg-blue-500">Remove</button>
           </div>
           
           <br><br>
           
           <div class="flex items-center mb-4" v-for="(project, index) in store.completeProject" :key="index">
             <p class="w-full text-red-500 line-through">{{ project.name }}</p>
-            <button
-            @click="store.doneAsInComplete(project.id)" type="button" class="p-2 ml-4 mr-2 border-2 rounded flex-no-shrink hover:text-white text-grey border-grey hover:bg-grey">Not
-              Done</button>
+            
+          <div @click="store.doneAsInComplete(project.id)" class="flex items-center justify-center w-6 h-6 p-1 mr-2 bg-white shadow">
+                <input type="checkbox"  checked>
+                <svg class="hidden w-4 h-4 text-green-600 pointer-events-none" viewBox="0 0 172 172"><g fill="none" stroke-width="none" stroke-miterlimit="10" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode:normal"><path d="M0 172V0h172v172z"/><path d="M145.433 37.933L64.5 118.8658 33.7337 88.0996l-10.134 10.1341L64.5 139.1341l91.067-91.067z" fill="currentColor" stroke-width="1"/></g></svg>
+            </div>
             <button type="button" @click="store.deleteProject(project.id)"
-              class="p-2 ml-2 border-2 rounded-ml flex-no-shrink text-red border-red hover:text-white hover:bg-blue-500">Remove</button>
+              class="p-2 ml-2 border-2 border-red-500 rounded-ml flex-no-shrink text-red hover:text-white hover:bg-blue-500">Remove</button>
           </div>
 
         </div>
